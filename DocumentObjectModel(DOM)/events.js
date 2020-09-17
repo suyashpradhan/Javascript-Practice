afa89dc32dab0f21bae48def98eeb8d6874e1f8b
suyashpradhan <pradhansuyash39@gmail.com>
2020-09-17 23:06:52 +0530

    #Day2 Learning Events #100DaysOfCode

--- DocumentObjectModel(DOM)/events.js
+++ DocumentObjectModel(DOM)/events.js
@@ -4,3 +4,8 @@ button.addEventListener('click', function (e) {
     document.body.style.backgroundColor = 'red';
     e.preventDefault();
 })
+
+
+button.addEventListener('click', function (e) {
+    alert("Button Clicked!");
+})
