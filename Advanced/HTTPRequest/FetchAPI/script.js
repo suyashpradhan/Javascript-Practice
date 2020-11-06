27e9ec3a78f281c8044569f3619192348f156a78
suyashpradhan <pradhansuyash39@gmail.com>
2020-11-06 17:55:04 +0530

    Fetch API #100DaysOfCode #Day51

---
+++ Advanced/HTTPRequest/FetchAPI/script.js
@@ -0,0 +1,29 @@
+//! USING XHR
+/* function generateJokesFunc() {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    const data = JSON.parse(xhr.responseText);
+
+    for (jokes of data.value) {
+      console.log(jokes.joke);
+    }
+  };
+
+  xhr.open("GET", `http://api.icndb.com/jokes/random/2`, true);
+  xhr.send();
+}
+generateJokesFunc(); */
+
+//? USING FETCH API
+fetch("http://aapi.icndb.com/jokes/random/2")
+  .then((response) => {
+    console.log(response);
+    response.json().then((any) => {
+      for (jokes of any.value) {
+        console.log(jokes.joke);
+      }
+    });
+  })
+  .catch((err) => {
+    console.log(`error `, err);
+  });
